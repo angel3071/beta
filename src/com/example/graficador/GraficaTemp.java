00c24e8f5955fb19c79052975813b93477c8a582
Arturo <arturo.cr88@gmail.com>
2013-07-04 00:47:07 -0500

    Nuevo tamaño de la DIV que contiene la gráfica.

--- src/com/example/graficador/GraficaTemp.java
+++ src/com/example/graficador/GraficaTemp.java
@@ -109,9 +109,7 @@ public class GraficaTemp extends Activity {
                 
                 
                 
-                progresBar.dismiss();
-                Toast.makeText(getApplicationContext(), "La grafica aparecera dentro de poco", Toast.LENGTH_LONG).show();
-                mWebView.loadUrl("javascript:grafica(\""+cadena+","+this.title+"\")");
+               
                 
                 
                 
